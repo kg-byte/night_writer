f8322e861d30fa9e06e3b1f949aece6d2c1e1443
Kim G <xiaole.guo@yahoo.com>
2022-03-04 16:00:21 -0700

    update night_writer.rb to print braille.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,8 +3,12 @@ require_relative 'braille_converter'
 eng_reader = File.open(ARGV[0], "r")
 eng_message = eng_reader.read
 eng_reader.close
-
-place_holder = eng_message.upcase
+braille_converter = BrailleConverter.new
+braille_message = braille_converter.convert(eng_message)
 braille_writer = File.open(ARGV[1], "w")
-braille_writer.write(place_holder)
+braille_message.each {|line|
+  braille_writer.write(line)
+  braille_writer.write("\n")
+                    }
+  braille_writer.close
 puts "Created 'braille.txt' containing #{eng_message.length} characters"
