510a9c9c523289a0b006a635a3ca8806b9ae2f13
Kim G <xiaole.guo@yahoo.com>
2022-03-04 16:41:57 -0700

    add test for cut_every_40 method

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -16,6 +16,16 @@ RSpec.describe 'night_writer' do
             expect(@braille_converter.convert('a')).to eq ["0.", "..", ".."]
             expect(@braille_converter.convert('hello')).to eq  ["0.0.0.0.0.", "00.00.0..0", "....0.0.0."]
           end
+
+          it 'cuts input every 40 characters' do
+            string = "a"*50
+            cut_lines = @braille_converter.cut_every_40_chars(string)
+            expect(cut_lines.count).to eq 2
+            expect(cut_lines[0].length).to eq 40
+            expect(cut_lines[1].length).to eq 10
+
+
+          end
         end
     end
 
