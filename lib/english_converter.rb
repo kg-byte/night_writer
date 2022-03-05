4e27c40817b1b7c98cdf66331ab9de37c0bc992b
Kim G <xiaole.guo@yahoo.com>
2022-03-04 18:41:51 -0700

    moved hash dictionary to new class library

---
+++ lib/english_converter.rb
@@ -0,0 +1,63 @@
+require_relative 'library'
+
+class EnglishConverter
+    def initialize
+      @_message = nil
+      @hash = Library.dictionary.invert
+      require 'pry'; binding.pry
+    end
+
+    # def translate(input)
+    #   if input.count > 1
+    #     lines = input.map{|line| line.split('')}
+    #     braille = lines.map {|line| line.map {|letter| @hash[letter.to_sym]}}
+    #   else
+    #     letters = input[0].split('')
+    #     braille = letters.map {|letter| @hash[letter.to_sym]}
+    #   end
+    #   braille
+    # end
+    #
+    #
+    # def cut_every_40_chars(input)
+    #   if input.length > 40
+    #     @eng_message = input.chars.each_slice(40).map(&:join)
+    #   else
+    #     @eng_message = [input]
+    #   end
+    # end
+    #
+    #
+    # def convert(eng_message)
+    #   cut_every_40_chars(eng_message.gsub("\n", ''))
+    #   translated_braille = translate(@eng_message)
+    #   if @eng_message.count == 1
+    #     output = output_braille_one_line(translated_braille)
+    #   else
+    #     output = output_braille_lines(translated_braille)
+    #   end
+    #   output
+    # end
+    #
+    # def output_braille_one_line(line)
+    #   lines = Array.new
+    #   lines[0] = line.flat_map{|letter|  letter[0]}.join
+    #   lines[1] = line.flat_map{|letter|  letter[1]}.join
+    #   lines[2] = line.flat_map{|letter|  letter[2]}.join
+    #   lines
+    # end
+    #
+    # def output_braille_lines(lines)
+    #   multi_lines = Array.new
+    #   i = 0
+    #   lines.each {|line|
+    #     i
+    #     multi_lines[i] = [line.flat_map{|letter|  letter[0]}.join,
+    #                              line.flat_map{|letter|  letter[1]}.join,
+    #                              line.flat_map{|letter|  letter[2]}.join]
+    #     i += 1
+    #   }
+    #   # require 'pry'; binding.pry
+    #   multi_lines
+    # end
+end
---
+++ lib/night_reader.rb
@@ -0,0 +1,24 @@
+require_relative 'english_converter'
+
+braille_reader = File.open(ARGV[0], "r")
+braille_message = braille_reader.read
+braille_reader.close
+require 'pry'; binding.pry
+english_converter = EnglishConverter.new
+english_message = english_converter.convert(braille_message)
+english_writer = File.open(ARGV[1], "w")
+# if english_message.all? {|element| element.class == String}
+#   english_message.each {|row|
+#   english_writer.write(row)
+#   english_writer.write("\n")
+#             }
+# else
+#   english_message.each{|line|
+#     line.each {|row|
+#     english_writer.write(row)
+#     english_writer.write("\n")
+#               }
+#                       }
+# end
+  english_writer.close
+puts "Created 'original_message.txt' containing #{eng_message.length-1} characters"
