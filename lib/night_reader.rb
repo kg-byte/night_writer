82c758595908be8d4ec481cd3962334e5f345b16
Kim G <xiaole.guo@yahoo.com>
2022-03-05 17:46:47 -0700

    correct error in library/update night_reader/iteration_3 complete

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -5,20 +5,8 @@ braille_message = braille_reader.read
 braille_reader.close
 # require 'pry'; binding.pry
 english_converter = EnglishConverter.new
-# english_message = english_converter.convert(braille_message)
+english_message = english_converter.convert(braille_message)
 english_writer = File.open(ARGV[1], "w")
-# if english_message.all? {|element| element.class == String}
-#   english_message.each {|row|
-#   english_writer.write(row)
-#   english_writer.write("\n")
-#             }
-# else
-#   english_message.each{|line|
-#     line.each {|row|
-#     english_writer.write(row)
-#     english_writer.write("\n")
-#               }
-#                       }
-# end
+english_writer.write(english_message)
   english_writer.close
-puts "Created 'original_message.txt' containing #{eng_message.length-1} characters"
+puts "Created 'original_message.txt' containing #{english_message.length} characters"
