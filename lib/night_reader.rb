0eb9ed04e76f151a7a1d925da764694e28c6e30d
Kim G <xiaole.guo@yahoo.com>
2022-03-07 12:09:25 -0700

    add night_runner class to simplify reader and writer runner

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,13 +1,3 @@
-require_relative 'english_converter'
+require_relative 'night_runner'
 
-braille_reader = File.open(ARGV[0], "r")
-braille_message = braille_reader.read
-braille_reader.close
-
-english_converter = EnglishConverter.new
-english_message = english_converter.convert(braille_message)
-english_writer = File.open(ARGV[1], "w")
-english_writer.write(english_message)
-english_writer.close
-
-puts "Created 'original_message.txt' containing #{english_message.length} characters"
+NightRunner.new.convert("NightReader")
---
+++ lib/night_runner.rb
@@ -0,0 +1,45 @@
+require_relative 'english_converter'
+require_relative 'braille_converter'
+
+class NightRunner
+  def initialize
+    @input = ''
+    @type = ''
+  end
+
+  def read
+    reader = File.open(ARGV[0], "r")
+    @input = reader.read
+    reader.close
+  end
+
+  def english_converter
+    english_converter = EnglishConverter.new
+    output = english_converter.convert(@input)
+    puts "Created 'original_message.txt' containing #{output.length} characters"
+    output
+  end
+
+  def braille_converter
+    braille_converter = BrailleConverter.new
+    puts "Created 'braille.txt' containing #{@input.length-1} characters"
+    braille_converter.convert(@input)
+  end
+
+    def convert(type)
+    read
+    if type == "NightReader"
+      output = english_converter
+    elsif type == "NightWriter"
+      output = braille_converter
+    end
+    write(output)
+  end
+
+  def write(message)
+    writer = File.open(ARGV[1], "w")
+    writer.write(message)
+    writer.close
+  end
+
+end
